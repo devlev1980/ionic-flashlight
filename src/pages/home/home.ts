1cd115f1b8cc4da2c3e911ecc58fbb8fcf6544fb
yevgenyl <yevgeny@consist.co.il>
2018-10-19 11:38:35 +0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Flashlight} from "@ionic-native/flashlight";
+import {Platform} from "ionic-angular";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+isOn: boolean ;
+  constructor(public navCtrl: NavController,private flashLight: Flashlight,
+              private platform: Platform ) {
+    this.platform.ready().then(()=>{
+      this.updateStatusOfFlashLight();
+    })
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  switchOnFlashLight(){
+    this.flashLight.switchOn();
+    this.updateStatusOfFlashLight();
+  }
+  switchOffFlashLight(){
+    this.flashLight.switchOff();
+    this.updateStatusOfFlashLight();
+  }
+  toggleFlashLight(){
+    this.flashLight.toggle();
+    this.updateStatusOfFlashLight()
+  }
+  updateStatusOfFlashLight(){
+    this.isOn = this.flashLight.isSwitchedOn();
   }
 
 }
